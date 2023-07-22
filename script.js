a3113c5d042133c9320dabd048d74c38a2fbbd07
akeel14 <akeellashley@gmail.com>
2023-07-22 10:15:45 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,112 @@
+// LESSON 1 - Intro to JavaScript, Control Flow, & Loops
+
+// 1. What is the difference between var, let and const in JavaScript?
+/* your code here */
+
+// 2. What are the different types of data in JavaScript?
+/* your code here */
+
+// 3. Write a for loop that logs numbers from 1 to 10 to the console.
+/* your code here */
+
+// 4. What is the difference between == and === in JavaScript?
+/* your code here */
+
+// 5. Write a simple 'if' statement that logs 'Hello, World!' to the console if 5 is greater than 2.
+/* your code here */
+
+// LESSON 2 - Functions & Scope
+
+// 6. Write a function named 'greet' that takes a name as a parameter and returns a greeting message.
+/* your code here */
+
+// 7. Explain what variable scope is.
+/* your code here */
+
+// 8. Create a variable in the global scope, and another in a function scope. Log them both to the console.
+/* your code here */
+
+// LESSON 3 - Data Structures, Object Methods & Enumeration
+
+// 9. What is an array in JavaScript? Give an example.
+/* your code here */
+
+// 10. Write a JavaScript object named 'person' with properties 'name' and 'age'.
+/* your code here */
+
+// 11. How do you access the properties of an object in JavaScript?
+/* your code here */
+
+// 12. Write a for-in loop that enumerates over the properties of the 'person' object.
+/* your code here */
+
+// LESSON 4 - Advanced Array Methods, Introduction to the DOM, Events
+
+// 13. What are the map() and filter() functions used for on arrays?
+/* your code here */
+
+// 14. Select the element with the ID 'myElement' using JavaScript.
+/* your code here */
+
+// 15. What is an event in JavaScript? Give an example.
+/* your code here */
+
+// LESSON 5 - Browser Events, Event Listeners, & Event Delegation
+
+// 16. Write a JavaScript function that adds an event listener to a button with the ID 'myButton', which logs 'Button clicked!' to the console.
+/* your code here */
+
+// 17. What is event delegation in JavaScript?
+/* your code here */
+
+// LESSON 7 - Asynchronous JavaScript & Intro to APIs and AJAX
+
+// 19. Fetch user data from 'https://jsonplaceholder.typicode.com/users' and log the response data to the console.
+/* your code here */
+
+// 20. Using the data fetched in the previous question, create a table in HTML using DOM manipulation. The table should have the following columns: 'ID', 'Name', 'Username', 'Email'. Use the map() function to create the table rows.
+/* your code here */
+
+// LESSON 8 - Promises & API Codealong
+
+// 20. What is a Promise in JavaScript? Give an example.
+/* your code here */
+
+// 21. Write a function that returns a Promise that resolves after 2 seconds and then console logs 'Promise resolved!'.
+/* your code here */
+
+// Debugging
+
+// 22. Below is a function with a bug. The function should log 'Hello, [name]!', but it isn't working. Debug and fix the issue.
+// Debugging question:
+function sayHello(name) {
+  console.log("Hello, " + Name);
+}
+
+sayHello("Alice");
+
+// Additional Questions
+
+// 23. Explain the concept of closure in JavaScript.
+/* your code here */
+
+// 24. How does 'this' keyword work in JavaScript?
+/* your code here */
+
+// 25. What is JSON and how is it used in JavaScript?
+/* your code here */
+
+// 26. Write a JavaScript function that takes an array of numbers and returns the sum.
+/* your code here */
+
+// 27. What is an anonymous function in JavaScript?
+/* your code here */
+
+// 28. What are JavaScript arrow functions and how do they differ from regular functions?
+/* your code here */
+
+// 29. Explain the concept of hoisting in JavaScript.
+/* your code here */
+
+// 30. Write a JavaScript function that reverses a string.
+/* your code here */
